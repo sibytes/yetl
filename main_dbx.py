a3c45fb900e2c5dcc8499f32732b741b757e3772
Shaun Ryan <Shaun_Chiburi@hotmail.com>
2022-07-30 09:36:51 +0100

    chore: databricks test

---
+++ main_dbx.py
@@ -0,0 +1,46 @@
+from yetl_flow import yetl_flow, IDataflow, Context
+from pyspark.sql.functions import *
+import logging
+from datetime import datetime
+
+@yetl_flow(log_level="ERROR")
+def customer_landing_to_rawdb_csv(
+    context: Context, dataflow: IDataflow, timeslice: datetime, timeslice_mask: str
+) -> dict:
+    """Load the demo customer data as is into a raw delta hive registered table.
+
+    this is a test pipeline that can be run just to check everything is setup and configured
+    correctly.
+    """
+    context.log.info(
+        f"Executing Dataflow {context.app_name} with timeslice={timeslice}, retries={dataflow.retries}"
+    )
+
+    # the config for this dataflow has 2 landing sources that are joined
+    # and written to delta table
+    # delta tables are automatically created and if configured schema exceptions
+    # are loaded syphened into a schema exception table
+    df_cust = dataflow.source_df("landing.customer")
+    df_prefs = dataflow.source_df("landing.customer_preferences")
+
+    df = df_cust.join(df_prefs, "id", "inner")
+    df = df_cust
+
+    dataflow.destination_df("raw.customer", df)
+
+
+
+logger = logging.getLogger(__name__)
+logger.setLevel(logging.INFO)
+
+
+logger.info(f"Executing pipeline test_customer_landing_to_rawdb")
+
+timeslice = datetime(2022, 1, 1, 0, 0, 0, 0)
+timeslice_mask = "*/*/*"
+results = customer_landing_to_rawdb_csv(
+    timeslice=timeslice, timeslice_mask=timeslice_mask
+)
+# results = pipeline.test_customer_landing_to_rawdb_csv()
+
+
