f787eb3fccfd6b36f542c93b4cbe29e025be2b26
Shaun Ryan <Shaun_Chiburi@hotmail.com>
2022-08-07 16:55:01 +0100

    feature: cdc upsert usecase

--- cdc_main.py
+++ cdc_main.py
@@ -1,7 +1,7 @@
 
 from yetl_flow import yetl_flow, IDataflow, Context, Timeslice, TimesliceUtcNow, OverwriteSave, Save
 from yetl_flow.dataset import Destination
-from pyspark.sql.functions import *
+from pyspark.sql import functions as fn
 from typing import Type
 from delta.tables import DeltaTable
 
@@ -15,14 +15,64 @@ def cdc_customer_landing_to_rawdb_csv(
     save_type: Type[Save] = None
 ) -> dict:
 
+    # get the source feed.
     df = dataflow.source_df("landing.cdc_customer")
+    df = (df.alias("src")
+          .withColumn("from_date", fn.expr("cast(extract_date as date)"))
+          .withColumn("to_date", fn.expr("cast(extract_date as date)"))
+          .withColumn("active", fn.lit(True))
+          .withColumn("data_name", fn.lit("src"))
+    )
 
+    # get the current destination
     dst_name = "raw.cdc_customer"
     dst:Destination = dataflow.destinations[dst_name]
-
-    context.log.info(f"Fetching dst delta table {dst_name} from {dst.path}")
     dst_table = DeltaTable.forPath(context.spark, dst.path)
     context.log.info(f"Fetched dst delta table {dst_name} from {dst.path}")
+
+
+    # get the history destination
+    dst_history_name = "raw.cdc_customer_history"
+    dst_history:Destination = dataflow.destinations[dst_history_name]
+    dst_table_history = DeltaTable.forPath(context.spark, dst_history.path)
+    context.log.info(f"Fetched dst delta table {dst_history_name} from {dst_history.path}")
+
+
+    # get all the incoming, matching current and historical recrods
+    # into a single dataframe
+    current_df = (
+        context.spark.sql(f"""
+            SELECT *
+            FROM {dst.database}.{dst.table}
+        """).alias("dst_c")
+        .join(df, "id", "inner")
+        .withColumn("data_name", fn.lit("dst_c"))
+        .select(
+            "dst_c.*", "data_name"
+        )
+    )
+    current_df.show()
+
+    historical_df = (
+        context.spark.sql(f"""
+            SELECT * 
+            FROM {dst.database}.{dst.table}_history
+        """).alias("dst_h")
+        .join(df, "id", "inner")
+        .withColumn("data_name", fn.lit("dst_c"))
+        .select("dst_h.*", "data_name")
+    )
+    # # union current with history
+    historical_df.show()
+
+    df_existing_destination = current_df.unionAll(historical_df)
+
+    df_existing_destination.show()
+    # df_change_set = df.unionAll(df_existing_destination)
+    # # set the change tracking columns of the change set.
+    # df_change_set.show()
+    context.log.info("stop")
+    
 
     # result =(
     #     dst_table.alias('people') \
