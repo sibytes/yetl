11c45133d45a251e36ef0a05cee16101dc15f5bf
Shaun Ryan <Shaun_Chiburi@hotmail.com>
2022-08-31 20:59:55 +0100

    chore: upgrade to spark 3.3 deltalake 2.1 dbx test

--- dbx_notebook.py
+++ dbx_notebook.py
@@ -10,37 +10,51 @@ from yetl_flow import (
     Timeslice,
     TimesliceUtcNow,
     OverwriteSave,
+    OverwriteSchemaSave,
     Save,
 )
 from pyspark.sql.functions import *
-import logging
 from typing import Type
 
 
-@yetl_flow()
+@yetl_flow(log_level="ERROR")
 def customer_landing_to_rawdb_csv(
     context: Context,
     dataflow: IDataflow,
     timeslice: Timeslice = TimesliceUtcNow(),
     save_type: Type[Save] = None,
-):
-    context.log.info(
-        f"Executing Dataflow {context.app_name} with timeslice={timeslice}, retries={dataflow.retries}"
-    )
+) -> dict:
+    """Load the demo customer data as is into a raw delta hive registered table.
 
-    # the config for this dataflow has 2 landing sources that are joined and written to delta table
+    this is a test pipeline that can be run just to check everything is setup and configured
+    correctly.
+    """
+
+    # the config for this dataflow has 2 landing sources that are joined
+    # and written to delta table
     # delta tables are automatically created and if configured schema exceptions
     # are loaded syphened into a schema exception table
     df_cust = dataflow.source_df("landing.customer")
     df_prefs = dataflow.source_df("landing.customer_preferences")
+
+    context.log.info("Joining customers with customer_preferences")
     df = df_cust.join(df_prefs, "id", "inner")
-    df = df_cust
+    df = df.withColumn("_partition_key", lit(2022))
+
     dataflow.destination_df("raw.customer", df)
 
 
-# if timeslice isn't provided will default to TimesliceUtcNow
-timeslice = Timeslice(2022, "*", "*")
-results = customer_landing_to_rawdb_csv(timeslice=timeslice)
+# incremental load
+# timeslice = Timeslice(2022, 7, 12)
+# results = customer_landing_to_rawdb_csv(
+#     timeslice = Timeslice(2022, 7, 12)
+# )
+
+# reload load
+
+results = customer_landing_to_rawdb_csv(
+    timeslice=Timeslice(2022, "*", "*"), save_type=OverwriteSchemaSave
+)
 
 
 # COMMAND ----------
