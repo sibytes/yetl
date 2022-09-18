4035a3a9a81cfe8ebe7363f46ff5b3753549d9f2
Shaun Ryan <Shaun_Chiburi@hotmail.com>
2022-09-18 09:12:09 +0100

    {fiddle: pydantic prototype}

---
+++ prototypes/proto_pydantic.py
@@ -0,0 +1,29 @@
+
+
+from pydantic import BaseModel
+
+class Name(BaseModel):
+    firstName: str
+    lastName: str
+
+class FullName(BaseModel):
+    fullName: str
+
+class User(BaseModel):
+    id: int
+    name: Name | FullName
+
+data1 = {
+   'id':1, 
+   "name": {'firstName': 'Foo', 'lastName': 'Bar'},
+}
+user = User(**data1)
+print(user.name.lastName)
+print(user.name.firstName)
+
+data2 = {
+   'id':1, 
+   "name": {'fullName': 'Foo Bar'},
+}
+user2 = User(**data2)
+print(user2.name.fullName)
