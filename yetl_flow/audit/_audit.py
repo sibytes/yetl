a2b44de887990d63efb88b4d6700819e0708fb09
Shaun Ryan <Shaun_Chiburi@hotmail.com>
2022-10-03 18:49:05 +0100

    feature: audit log results

--- yetl_flow/audit/_audit.py
+++ yetl_flow/audit/_audit.py
@@ -98,7 +98,7 @@ class Audit:
 
     def _increment_count(self, level: AuditLevel):
         if self.audit_log[level.value].get(self._COUNT):
-            self.audit_log[level.value][self._COUNT] = +1
+            self.audit_log[level.value][self._COUNT] += 1
 
     def get(self, format: AuditFormat = AuditFormat.JSON):
 
