b9b97c7402221f55e90eec6ab4f9ab2e65227ccb
Shaun Ryan <Shaun_Chiburi@hotmail.com>
2022-11-20 10:18:19 +0000

    feature: pydantic refactor

--- yetl/flow/dataset/_reader.py
+++ yetl/flow/dataset/_reader.py
@@ -10,7 +10,8 @@ import uuid
 from ._base import Source
 from pyspark.sql import DataFrame
 from .._timeslice import Timeslice
-
+from pydantic import BaseModel, Field
+from enum import Enum
 
 def _yetl_properties_dumps(obj: dict, *, default):
     """Decodes the data back into a dictionary with yetl configuration properties names"""
@@ -19,6 +20,42 @@ def _yetl_properties_dumps(obj: dict, *, default):
     }
     return json.dumps(obj, default=default)
 
+class ReadModeOptions(Enum):
+    PERMISSIVE = "PERMISSIVE"
+    FAILFAST = "FAILFAST"
+    DROPMALFORMED = "DROPMALFORMED"
+    BADRECORDSPATH = "badRecordsPath"
+
+
+class ThresholdLimit(BaseModel):
+    min_rows: int = Field(default=0)
+    max_rows: int = Field(default=None)
+    exception_count: int = Field(default=0)
+    exception_percent: int = Field(default=0)
+
+
+class Read(BaseModel):
+    def __init__(self, **data: Any) -> None:
+        super().__init__(**data)
+        self.mode = self.options.get("mode", ReadModeOptions.PERMISSIVE)
+        self.infer_schema = self.options.get("inferSchema", False)
+
+    _DEFAULT_OPTIONS = {"mode": ReadModeOptions.PERMISSIVE.value, "inferSchema": False}
+    auto: bool = Field(default=True)
+    options: Dict[str, Any] = Field(default=_DEFAULT_OPTIONS)
+    mode: ReadModeOptions = Field(default=ReadModeOptions.PERMISSIVE)
+    infer_schema: bool = Field(default=False)
+
+
+class Exceptions(BaseModel):
+    path: str = Field(...)
+    database: str = Field(...)
+    table: str = Field(...)
+
+
+class Thresholds(BaseModel):
+    warning: ThresholdLimit = Field(default=ThresholdLimit())
+    error: ThresholdLimit = Field(default=ThresholdLimit())
 
 class Reader(Source):
     def __init__(self, **data: Any) -> None:
