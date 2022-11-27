43a13e14a38020089c65beae0ea53114642763d1
Shaun Ryan <Shaun_Chiburi@hotmail.com>
2022-11-27 10:38:11 +0000

    feature: pydantic refactor

--- test/fixtures_config.py
+++ test/fixtures_config.py
@@ -1,7 +1,11 @@
 import pytest
 import yaml
 import os
-
+from yetl.flow.context import SparkContext
+from yetl.flow.audit import Audit
+from yetl.flow.dataset import Reader
+from typing import Callable
+from yetl.flow._environment import Environment
 
 def load_config(name: str) -> dict:
     path = os.path.curdir
@@ -13,17 +17,35 @@ def load_config(name: str) -> dict:
 
     return config
 
-
 @pytest.fixture
 def i_context_config():
     return load_config("i_context")
-
 
 @pytest.fixture
 def spark_context_config():
     return load_config("spark_context")
 
-
 @pytest.fixture
 def databricks_context_config():
     return load_config("databricks_context")
+
+@pytest.fixture
+def spark_context(spark_context_config:dict):
+    environment = Environment()
+    context = SparkContext(
+        auditor=Audit(),
+        project="demo",
+        name="demo",
+        environment=environment,
+        **spark_context_config
+    )
+    return context
+    
+@pytest.fixture
+def reader_dataset_config():
+    return load_config("reader_dataset")
+
+@pytest.fixture
+def reader_dataset(reader_dataset_config:dict, spark_context:SparkContext):
+    reader = Reader(context=spark_context, **reader_dataset_config)
+    return reader
--- test/test_dataset_reader.py
+++ test/test_dataset_reader.py
@@ -1,99 +1,25 @@
 from yetl.flow.dataset import Reader
 import json
 from unittest import TestCase
+from .fixtures_config import *
 
-reader_config = {
-    "timeslice": {
-        "year": "*",
-        "month": "*",
-        "day": "*",
-        "hour": 0,
-        "minute": 0,
-        "second": 0,
-        "microsecond": 0,
-    },
-    "catalog": None,
-    "context_id": "bf76fc9b-94d0-4e86-bf80-7e74893dd487",
-    "dataflow_id": "26658212-b2b4-4aaa-8173-393829d57bd2",
-    "dataframe": None,
-    "dataset_id": "decb22d6-1dee-450c-8c20-9b09b6684159",
-    "datalake_protocol": "file:",
-    "datalake": "c/mylake",
-    "database": "customer",
-    "table": "landing",
-    "properties": {
-        "yetl.schema.createIfNotExists": True,
-        "yetl.schema.corruptRecordName": "_corrupt_record",
-        "yetl.schema.corruptRecord": False,
-        "yetl.metadata.contextId": True,
-        "yetl.metadata.dataflowId": True,
-        "yetl.metadata.datasetId": True,
-        "yetl.metadata.timeslice": "timeslice_file_date_format",
-        "yetl.metadata.filepathFilename": True,
-        "yetl.metadata.filepath": True,
-        "yetl.metadata.filename": True,
-    },
-    "path_date_format": "%Y%m%d",
-    "file_date_format": "%Y%m%d",
-    "format": "csv",
-    "path": "landing/demo/{{ timeslice_path_date_format }}/customer_details_{{ timeslice_file_date_format }}.csv",
-    "read": {
-        "auto": True,
-        "infer_schema": True,
-        "mode": "PERMISSIVE",
-        "options": {"mode": "PERMISSIVE", "inferSchema": True, "header": True},
-    },
-    "exceptions": {
-        "path": "delta_lake/demo_landing/{{table_name}}_exceptions",
-        "database": "demo_landing",
-        "table": "{{table_name}}_exceptions",
-    },
-    "thresholds": {
-        "warning": {
-            "min_rows": 1,
-            "max_rows": 1000,
-            "exception_count": 0,
-            "exception_percent": 0,
-        },
-        "error": {
-            "min_rows": 0,
-            "max_rows": 100000000,
-            "exception_count": 50,
-            "exception_percent": 80,
-        },
-    },
-}
+def test_reader_sql_database_table(reader_dataset:Reader, reader_dataset_config:dict):
 
+    database = reader_dataset_config["database"]
+    table = reader_dataset_config["table"]
 
-def test_reader():
-
-    reader = Reader.parse_obj(reader_config)
-    actual: dict = json.loads(reader.json())
-    expected = reader_config
-    expected["path"] = "file:c/mylake/landing/demo/***/customer_details_***.csv"
-
-    test = TestCase()
-    test.maxDiff = None
-    test.assertDictEqual(expected, actual)
-
-
-def test_reader_database_table():
-
-    reader = Reader.parse_obj(reader_config)
-    database = reader_config["database"]
-    table = reader_config["table"]
     expected = f"`{database}`.`{table}`"
-    actual = reader.sql_database_table
-
+    actual = reader_dataset.sql_database_table
     assert expected == actual
 
 
-def test_reader_database_table():
 
-    reader = Reader.parse_obj(reader_config)
-    database = reader_config["database"]
-    table = reader_config["table"]
+def test_reader_database_table(reader_dataset:Reader, reader_dataset_config:dict):
+
+    database = reader_dataset_config["database"]
+    table = reader_dataset_config["table"]
+
     expected = f"{database}.{table}"
-    actual = reader.database_table
-
+    actual = reader_dataset.database_table
     assert expected == actual
+
