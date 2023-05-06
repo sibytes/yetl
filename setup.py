570bc9f2690db643a4a6b2c82b5e902bf4e7ea6a
Shaun Ryan <shaunryan@WorlordV.local>
2023-05-06 18:03:15 +0100

    defaulting timeslice

--- setup.py
+++ setup.py
@@ -10,7 +10,7 @@ README = (HERE / "README.md").read_text()
 # This call to setup() does all the work
 setup(
     name="yetl-framework",
-    version="1.1.1",
+    version="1.1.0",
     description="yet (another spark) etl framework",
     long_description=README,
     long_description_content_type="text/markdown",
@@ -29,8 +29,8 @@ setup(
     ],
     packages=[
         "yetl",
-        "yetl.dataset",
-        "yetl.workflow"
+        "yetl.workflow",
+        "yetl.table"
     ],
     install_requires=[
           'PyYAML',
--- yetl/_config.py
+++ yetl/_config.py
@@ -10,12 +10,18 @@ from ._project import Project
 
 class Config:
     def __init__(
-        self, project: str, pipeline: str, timeslice: Timeslice = None, config_path: str = None
+        self,
+        project: str,
+        pipeline: str,
+        timeslice: Timeslice = None,
+        config_path: str = None,
     ):
         self.config_path = get_config_path(project, config_path)
         configure_logging(project, self.config_path)
         if not timeslice:
-            timeslice = Timeslice(year="*", month="*", day="*")
+            timeslice = Timeslice(
+                year=_INDEX_WILDCARD, month=_INDEX_WILDCARD, day=_INDEX_WILDCARD
+            )
         self.project = self._load_project(project)
         self.pipeline = pipeline
         self.tables = self._load_tables(timeslice)
