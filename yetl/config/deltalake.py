9d58e8948a81b8c407bc33011bf555f6721c8921
Shaun Ryan <shaunryan@WorlordV.local>
2023-05-11 09:03:01 +0100

    migrated date striping parser

--- yetl/config/deltalake.py
+++ yetl/config/deltalake.py
@@ -9,7 +9,7 @@ from pydantic import BaseModel, Field
 from typing import Any
 from ._project import Project
 from pyspark.sql import SparkSession
-
+import re
 
 _logger = logging.getLogger(__name__)
 
@@ -21,6 +21,182 @@ class DeltaLakeFn(BaseModel):
 
     project: Project = Field(...)
     spark: SparkSession = Field(default=None)
+
+    @classmethod
+    def to_regex_search_pattern(cls, py_format: str):
+        """Convert python format codes to regex search pattern
+           these can be used to intelligently strip timeslice from file or name
+           see here for an exampl
+           https://github.com/sibytes/yetl_archive/blob/main/yetl/flow/dataset/_reader.py#L387
+
+            (\d{1,4}([.\-/])\d{1,2}([.\-/])\d{1,4})  # noeq W605
+        https://spark.apache.org/docs/latest/sql-ref-datetime-pattern.html
+
+        %d -> \d{2}       - Day of the month as a zero-padded decimal number.
+        %m -> [0-1][1-9]  - Month as a zero-padded decimal number.
+        %y -> \d{2}       - Year without century as a zero-padded decimal number.
+        %Y -> \d{4}       - Year with century as a decimal number.
+        %H -> [0-2][1-9]  - Hour (24-hour clock) as a zero-padded decimal number.
+        %M -> \d{2}       - Minute as a zero-padded decimal number.
+        %S -> \d{2}       - Second as a zero-padded decimal number.
+        %f -> [0]\.?\d{6} - Microsecond as a decimal number, zero-padded to 6 digits.
+        %% -> ([%])       - A literal '%' character.
+        %j -> \d{2}       - Day of the year as a zero-padded decimal number.
+
+        NOT SUPPORTED - %U - Week number of the year (Sunday as the first day of the week) as a zero-padded decimal number. All days in a new year preceding the first Sunday are considered to be in week 0.
+        NOT SUPPORTED - %W - Week number of the year (Monday as the first day of the week) as a zero-padded decimal number. All days in a new year preceding the first Monday are considered to be in week 0.
+
+        NOT SUPPORTED - %c - Locale’s appropriate date and time representation.
+        NOT SUPPORTED - %x - Locale’s appropriate date representation.
+        NOT SUPPORTED - %X - Locale’s appropriate time representation.
+        NOT SUPPORTED - %G - ISO 8601 year with century representing the year that contains the greater part of the ISO week (%V).
+        NOT SUPPORTED - %u - ISO 8601 weekday as a decimal number where 1 is Monday.
+        NOT SUPPORTED - %V - ISO 8601 week as a decimal number with Monday as the first day of the week. Week 01 is the week containing Jan 4.
+        NOT SUPPORTED - %z - UTC offset in the form ±HHMM[SS[.ffffff]] (empty string if the object is naive).
+        NOT SUPPORTED - %Z - Time zone name (empty string if the object is naive).
+        NOT SUPPORTED - %I - Hour (12-hour clock) as a zero-padded decimal number.
+        NOT SUPPORTED - %p - Locale’s equivalent of either AM or PM.
+        NOT SUPPORTED - %b - Month as locale’s abbreviated name.
+        NOT SUPPORTED - %B - Month as locale’s full name.
+        NOT SUPPORTED - %a - Weekday as locale’s abbreviated name.
+        NOT SUPPORTED - %A - Weekday as locale’s full name.
+        NOT SUPPORTED - %w - Weekday as a decimal number, where 0 is Sunday and 6 is Saturday.
+        """  # noqa W605
+
+        UNSUPPORTED_FORMAT_CODES = [
+            "%U",
+            "%W",
+            "%c",
+            "%x",
+            "%X",
+            "%G",
+            "%u",
+            "%V",
+            "%z",
+            "%Z",
+            "%I",
+            "%p",
+            "%b",
+            "%B",
+            "%a",
+            "%A",
+            "%w",
+        ]
+        unsupported_codes = []
+        for c in UNSUPPORTED_FORMAT_CODES:
+            if c in py_format:
+                unsupported_codes.append(c)
+
+        if unsupported_codes:
+            unsupported_codes = ",".join(unsupported_codes)
+            raise Exception(
+                f"The format contains the following unsupported format codes: {unsupported_codes}"
+            )
+
+        pattern = py_format.replace("%d", "\d{2}")  # noqa W605
+        pattern = pattern.replace("%m", "[0-1][1-9]")  # noqa W605
+        pattern = pattern.replace("%y", "\d{2}")  # noqa W605
+        pattern = pattern.replace("%Y", "\d{4}")  # noqa W605
+        pattern = pattern.replace("%H", "[0-2][1-9]")  # noqa W605
+        pattern = pattern.replace("%M", "\d{2}")  # noqa W605
+        pattern = pattern.replace("%S", "\d{2}")  # noqa W605
+        pattern = pattern.replace("%f", "[0]\.?\d{6}")  # noqa W605
+
+        seps = [f"%{s[0]}" for s in pattern.split("%")][1:]
+        replace_seps = [s[0] for s in pattern.split("%")][1:]
+        replace_seps = [f"[{s}]" for s in replace_seps]
+
+        for s, r in zip(seps, replace_seps):
+            pattern = pattern.replace(s, r)
+
+        pattern = f"({pattern})"
+
+        return pattern
+
+    @classmethod
+    def to_spark_format_code(cls, py_format: str):
+        """Convert python format codes to spark format codes
+
+        https://spark.apache.org/docs/latest/sql-ref-datetime-pattern.html
+
+        %d -> dd    - Day of the month as a zero-padded decimal number.
+        %m -> MM    - Month as a zero-padded decimal number.
+        %y -> yy    - Year without century as a zero-padded decimal number.
+        %Y -> yyyy  - Year with century as a decimal number.
+        %H -> HH    - Hour (24-hour clock) as a zero-padded decimal number.
+        %M -> mm    - Minute as a zero-padded decimal number.
+        %S -> s     - Second as a zero-padded decimal number.
+        %f -> S     - Microsecond as a decimal number, zero-padded to 6 digits.
+        %% -> %     - A literal '%' character.
+        %j -> DD    - Day of the year as a zero-padded decimal number.
+
+        NOT SUPPORTED - %U - Week number of the year (Sunday as the first day of the week) as a zero-padded decimal number. All days in a new year preceding the first Sunday are considered to be in week 0.
+        NOT SUPPORTED - %W - Week number of the year (Monday as the first day of the week) as a zero-padded decimal number. All days in a new year preceding the first Monday are considered to be in week 0.
+
+        NOT SUPPORTED - %c - Locale’s appropriate date and time representation.
+        NOT SUPPORTED - %x - Locale’s appropriate date representation.
+        NOT SUPPORTED - %X - Locale’s appropriate time representation.
+        NOT SUPPORTED - %G - ISO 8601 year with century representing the year that contains the greater part of the ISO week (%V).
+        NOT SUPPORTED - %u - ISO 8601 weekday as a decimal number where 1 is Monday.
+        NOT SUPPORTED - %V - ISO 8601 week as a decimal number with Monday as the first day of the week. Week 01 is the week containing Jan 4.
+        NOT SUPPORTED - %z - UTC offset in the form ±HHMM[SS[.ffffff]] (empty string if the object is naive).
+        NOT SUPPORTED - %Z - Time zone name (empty string if the object is naive).
+        NOT SUPPORTED - %I - Hour (12-hour clock) as a zero-padded decimal number.
+        NOT SUPPORTED - %p - Locale’s equivalent of either AM or PM.
+        NOT SUPPORTED - %b - Month as locale’s abbreviated name.
+        NOT SUPPORTED - %B - Month as locale’s full name.
+        NOT SUPPORTED - %a - Weekday as locale’s abbreviated name.
+        NOT SUPPORTED - %A - Weekday as locale’s full name.
+        NOT SUPPORTED - %w - Weekday as a decimal number, where 0 is Sunday and 6 is Saturday.
+        """
+
+        UNSUPPORTED_FORMAT_CODES = [
+            "%U",
+            "%W",
+            "%c",
+            "%x",
+            "%X",
+            "%G",
+            "%u",
+            "%V",
+            "%z",
+            "%Z",
+            "%I",
+            "%p",
+            "%b",
+            "%B",
+            "%a",
+            "%A",
+            "%w",
+        ]
+        unsupported_codes = []
+        for c in UNSUPPORTED_FORMAT_CODES:
+            if c in py_format:
+                unsupported_codes.append(c)
+
+        if unsupported_codes:
+            unsupported_codes = ",".join(unsupported_codes)
+            raise Exception(
+                f"The format contains the following unsupported format codes: {unsupported_codes}"
+            )
+
+        py_format = py_format.replace("%d", "dd")
+        py_format = py_format.replace("%m", "MM")
+        py_format = py_format.replace("%y", "yy")
+        py_format = py_format.replace("%Y", "yyyy")
+        py_format = py_format.replace("%H", "HH")
+        py_format = py_format.replace("%M", "mm")
+        py_format = py_format.replace("%S", "s")
+        py_format = py_format.replace("%f", "S")
+        py_format = py_format.replace("%", "")
+
+        return py_format
+
+    @classmethod
+    def reduce_whitespace(cls, sentence: str):
+        pattern = re.compile(r"\s+")
+
+        return pattern.sub(" ", sentence).strip()
 
     def get_partition_predicate(self, partition_values: dict):
         predicates = []
