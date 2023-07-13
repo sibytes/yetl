cf0a74461a179406fd8d4726594453ca212aff1b
Shaun Ryan <shaunryan@WorlordV.local>
2023-07-13 18:12:00 +0100

    json schema and resource bundling

--- setup.py
+++ setup.py
@@ -25,7 +25,7 @@ setup(
     classifiers=[
         "License :: OSI Approved :: MIT License",
         "Programming Language :: Python :: 3",
-        "Programming Language :: Python :: 3.8",
+        "Programming Language :: Python :: 3.10",
     ],
     packages=[
         "yetl",
