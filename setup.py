10c6164abb97d590a588040952634ed4ae1eb5fe
Shaun Ryan <shaunryan@WorlordV.local>
2023-07-15 18:47:39 +0100

    3.10 compatability

--- setup.py
+++ setup.py
@@ -25,6 +25,8 @@ setup(
     classifiers=[
         "License :: OSI Approved :: MIT License",
         "Programming Language :: Python :: 3",
+        "Programming Language :: Python :: 3.8",
+        "Programming Language :: Python :: 3.9",
         "Programming Language :: Python :: 3.10",
     ],
     include_package_data=True,
