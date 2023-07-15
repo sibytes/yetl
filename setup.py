0a79eb84232ea5dd384f19aa32ee121dbc5373dd
Shaun Ryan <shaunryan@WorlordV.local>
2023-07-15 19:11:07 +0100

    3.10 compatability

--- setup.py
+++ setup.py
@@ -10,7 +10,7 @@ README = (HERE / "README.md").read_text()
 # This call to setup() does all the work
 setup(
     name="yetl-framework",
-    version="1.7.4",
+    version="1.7.4.dev1",
     description="yet (another spark) etl framework",
     long_description=README,
     long_description_content_type="text/markdown",
